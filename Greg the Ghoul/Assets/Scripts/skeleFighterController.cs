ba155a97ff4babf0203c70af3fd341b11f6ec7a5
Brandond <bdude1887@gmail.com>
2016-09-25 15:26:58 -0400

    added controller for skeleton fighter

---
+++ Greg the Ghoul/Assets/Scripts/skeleFighterController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class skeleFighterController : MonoBehaviour {
+	public Animator anim;
+	private float moveH = 0;
+	private float moveV = 0;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
