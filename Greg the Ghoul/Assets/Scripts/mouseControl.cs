478ee2f8bb6228b512019bcc261df17a5d0bceb8
Brandond <bdude1887@gmail.com>
2016-09-25 16:44:31 -0400

    begin reworking the camera controls

--- Greg the Ghoul/Assets/Scripts/mouseControl.cs
+++ Greg the Ghoul/Assets/Scripts/mouseControl.cs
@@ -6,25 +6,25 @@ public class mouseControl : MonoBehaviour {
     Vector2 smoothV;
     public float sensitivity = 5.0f;
     public float smoothing = 2.0f;
-    GameObject ghoul;
+	public Transform target;
 	// Use this for initialization
 	void Start () {
-        ghoul = this.transform.parent.gameObject;
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		/*
         var md = new Vector2(Input.GetAxisRaw("Mouse X"),Input.GetAxisRaw("Mouse Y"));
 
         md = Vector2.Scale(md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
         smoothV.x = Mathf.Lerp(smoothV.x, md.x, 1f / smoothing);
         smoothV.y = Mathf.Lerp(smoothV.y, md.y, 1f / smoothing);
         mouseLook += smoothV;
-        mouseLook.y = Mathf.Clamp(mouseLook.y, 0f, 0f);
        
-
+        //mouseLook.y = Mathf.Clamp(mouseLook.y, 0f, 0f);
         transform.localRotation = Quaternion.AngleAxis(mouseLook.y, Vector3.right);
         ghoul.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, ghoul.transform.up);
-      
+		*/
+		transform.LookAt(target);
 	}
 }
