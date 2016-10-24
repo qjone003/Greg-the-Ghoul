4715249a2ca849c38add5762a10cda6e406d192e
Brandon <bdude1887@gmail.com>
2016-10-24 11:30:25 -0400

    Worked on aggro controller script

--- Greg the Ghoul/Assets/Scripts/ai/aggroController.cs
+++ Greg the Ghoul/Assets/Scripts/ai/aggroController.cs
@@ -8,6 +8,10 @@ public class aggroController : MonoBehaviour {
 	public Collider followRange;
 	public GameObject self;
 	public bool detected = false;
+	public bool seen = false;
+	public bool aware = false;
+	public GameObject aggroTarget;
+	public bool aggro = false;
 	public string[] enemies = {"Player"};
 	
 	void OnTriggerEnter(Collider other){
@@ -15,6 +19,7 @@ public class aggroController : MonoBehaviour {
 		for(int i = 0; i < enemies.Length; i++){
 			if(other.gameObject.tag == enemies[i]){
 				notEnemy = false;
+				aggroTarget = other.gameObject;
 			}
 		}
 		if(notEnemy){
@@ -26,7 +31,7 @@ public class aggroController : MonoBehaviour {
 			detected = true;
 			followRange.gameObject.SetActive(true);
 			detectRange.gameObject.SetActive(false);
-			Debug.Log("Hi!");
+			//Debug.Log("Hi!");
 		}
 		
 	}
@@ -38,9 +43,33 @@ public class aggroController : MonoBehaviour {
 		followRange.gameObject.SetActive(false);
 		detectRange.gameObject.SetActive(true);
 		detected = false;
-		Debug.Log("Bye!");
+		//Debug.Log("Bye!");
 	}
 	
 	void Start () {
+	}
+	
+	void Update () {
+		if(detected && seen){
+			//Within detection range and in sight
+			aggro = true;
+			aware = true;
+		}
+		if(detected && !seen){
+			//Within detection range but not in sight
+			if(aware){
+				//Knows that they are nearby and has seen them
+				aggro = true;
+			}
+			else{
+				//Hasn't seen them yet but they are nearby
+				aggro = false;
+			}
+		}
+		if(!detected){
+			//Not within detection range
+			aware = false;
+			aggro = false;
+		}
 	}
 }
