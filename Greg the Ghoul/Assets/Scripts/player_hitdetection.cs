883e445d5255e714fe5792c688883d1e60ecc843
Quinn Jones <quinnjns3@gmail.com>
2016-10-28 12:58:02 -0400

    Added health and mana bar to UI, started new scene with ghoul prefab to test UI designs. Added hit detection script for ghoul and started incorporation to UI.

---
+++ Greg the Ghoul/Assets/Scripts/player_hitdetection.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_hitdetection : MonoBehaviour {
+	
+	public Slider health;
+	private bool youDead = false;
+
+	// Update is called once per frame
+	void Update () {
+		if(!youDead){
+			Debug.Log("Checkpoint screen here");
+		}
+	
+	}
+	void OnTriggerEnter(Collider other){
+		if(other.gameObject.tag =="damage_source"){
+			health.value -= .011f;
+		}
+		else{
+			youDead = true;
+		}
+		
+	}
+}
