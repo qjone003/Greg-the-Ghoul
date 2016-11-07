2c58fd0c13ec4c3ee156da09df78568a4ebc61d5
Brandon <bdude1887@gmail.com>
2016-11-07 15:22:41 -0500

    Added layers for obstacles and the ground for making pathfinding nodes.  Implemented pathfinding.  The skeleton now uses pathfinding.  Tweaked the pathfinding to be a bit less jittery.  Tweaked the pathfinding to prevent spinning in place when at the current interest.

--- Greg the Ghoul/Assets/Scripts/ai/enemyBaseController.cs
+++ Greg the Ghoul/Assets/Scripts/ai/enemyBaseController.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+using Pathfinding;
+
 public class enemyBaseController : MonoBehaviour {
 	public Animator anim;
 	public float moveV = 0f;
@@ -14,6 +16,13 @@ public class enemyBaseController : MonoBehaviour {
 	public float runMultiplier = 1.5f;
 	public float minRunDistance = 3.5f;
 	public float minWalkDistance = 1f;
+	public GameObject lastLocation;
+	
+	//pathing
+	public Path path;
+	public float speed = 100;
+	public float nextWaypointDistance = 3;
+	private int currentWaypoint = 0;
 	
 	// Call this to remotely set interest
 	public void SetInterest(GameObject other){
@@ -21,11 +30,15 @@ public class enemyBaseController : MonoBehaviour {
 	}
 	public void SetInterest(){
 		if(seeAggroTarget){
-			SetInterest(aggroDetector.GetComponent<aggroController>().aggroTarget);
+			if(interest != aggroDetector.GetComponent<aggroController>().aggroTarget){
+				SetInterest(aggroDetector.GetComponent<aggroController>().aggroTarget);
+			}
 		}
 		else{
 			//TODO determine something to use as interest and set it
-			SetInterest(self);
+			if(interest != lastLocation){
+				SetInterest(lastLocation);
+			}
 			//Do a bunch of raycasts?  Expand a collision box?
 		}
 		//Debug.Log(interest.name);
@@ -35,7 +48,7 @@ public class enemyBaseController : MonoBehaviour {
 	public void MoveTo (GameObject other){
 		toTarget = other.transform.position - self.transform.position;
 		float translation = 0f;
-		Debug.Log(toTarget.magnitude);
+		//Debug.Log(toTarget.magnitude);
 		if(seeAggroTarget){
 			//run up to interest then slow down
 			if(toTarget.magnitude > minRunDistance){
@@ -67,18 +80,52 @@ public class enemyBaseController : MonoBehaviour {
 		else{
 			moveH = 0f;
 		}
-		translation *= Time.deltaTime;
+		
+		//Update the path after every waypoint
+		if(currentWaypoint >= 4 || currentWaypoint >= path.vectorPath.Count){
+			self.GetComponent<Seeker>().StartPath(transform.position, other.transform.position, OnPathComplete);
+		}
+		MoveOnPath();
+	}
+	
+	public void OnPathComplete (Path p) {
+		if (!p.error) {
+			path = p;
+			currentWaypoint = 0;
+		}
+	}
+	// Pathing
+	private void MoveOnPath () {
+		if (path == null){
+			Debug.Log("NO PATH!");
+			return;
+			//there is no path
+			//This should never happen
+		}
+		if (currentWaypoint >= path.vectorPath.Count){
+			currentWaypoint++;
+			return;
+			//reached end of path
+		}
 		
 		//Rotate the character
-		Vector3 tempTarget = new Vector3(	other.transform.position.x,
-											transform.position.y,
-											other.transform.position.z);
-		transform.LookAt(tempTarget);
-		//Move the character towards the GameObject
-		Vector3 tempTrans = transform.forward * translation;
-		self.GetComponent<CharacterController>().SimpleMove(tempTrans);
+		Vector3 tempV = new Vector3(interest.transform.position.x,
+									transform.position.y,
+									interest.transform.position.z
+									);
+		transform.LookAt(tempV);
 		
-		Debug.Log(tempTrans);
+		//Direction to the next waypoint
+		Vector3 dir = (path.vectorPath[currentWaypoint]-transform.position).normalized;
+		dir *= speed * Time.deltaTime;
+		self.GetComponent<CharacterController>().SimpleMove(dir);
+		
+		//Move towards next waypoint if close enough
+		if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) <
+		nextWaypointDistance) {
+			currentWaypoint++;
+			return;
+		}
 	}
 	
 	// Call this to attack
@@ -104,8 +151,10 @@ public class enemyBaseController : MonoBehaviour {
 	// Use this for initialization
 	public virtual void Start () {
 		interest = self;
+		self.GetComponent<Seeker>().StartPath(transform.position, interest.transform.position, OnPathComplete);
 	}
 	public virtual void Update(){
+		
 		seeAggroTarget = aggroDetector.GetComponent<aggroController>().seen;
 		SetInterest();
 		MoveTo(interest);
